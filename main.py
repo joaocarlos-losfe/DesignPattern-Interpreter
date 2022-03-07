707c70d8386a87e6e415099d7f0ec19a31ca2023
joaocarlos-losfe <joaocarlos.losfe@gmail.com>
2022-03-07 15:45:08 -0500

    Padrão de projeto Interpreter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import abc
 
+
+# "interface"
 class AbsExpressao(abc.ABC):
     @abc.abstractmethod
     def interpretar(self):
@@ -13,7 +15,7 @@ class Numero(AbsExpressao):
     def interpretar(self) -> int or float:
         return self.__numero
 
-#operaração não terminal
+#operaração não terminal: composta por outras 2 ou mais expressoes
 class Soma(AbsExpressao):
     def __init__(self, expressao_esquerda : Numero, expressao_direita : Numero):
         self.__expressao_esquerda = expressao_esquerda
@@ -43,7 +45,7 @@ class Multiplicar(AbsExpressao):
 if __name__ == '__main__':
 
     # ((5 + 5 - 3) * 10) + 10
-
+    #arvore ou pilha de expressoes
     resultado = Soma(Multiplicar(Subtracao(Soma(Numero(5), Numero(5)), Numero(3)), Numero(10)), Numero(10))
 
     print(resultado.interpretar())
