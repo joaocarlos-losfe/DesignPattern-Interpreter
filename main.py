2a6e8b95302c43663ddcdf26fc1d74b640340b1c
joaocarlos-losfe <joaocarlos.losfe@gmail.com>
2022-03-12 09:51:07 -0500

    aprimoramento de código

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class AbsExpressao(abc.ABC):
         pass
 
 #expressão terminal
-class Numero(AbsExpressao): 
+class Digito(AbsExpressao): 
     def __init__(self, numero:int or float):
         self.__numero = numero
 
@@ -16,38 +16,37 @@ class Numero(AbsExpressao):
 
 #operaração não terminal: composta por outras 2 ou mais expressoes
 class Soma(AbsExpressao):
-    def __init__(self, expressao_esquerda : Numero, expressao_direita : Numero):
+    def __init__(self, expressao_esquerda:Digito, expressao_direita:Digito):
         self.__expressao_esquerda = expressao_esquerda
         self.__expressao_direita = expressao_direita
 
     #interpreta a operaçao do lado esquedo e do lado direito 
-    def interpretar(self) -> Numero:
+    def interpretar(self):
         return self.__expressao_esquerda.interpretar() + self.__expressao_direita.interpretar()
 
 class Subtracao(AbsExpressao):
-    def __init__(self, expressao_esquerda: Numero, expressao_direita: Numero):
+    def __init__(self, expressao_esquerda:Digito, expressao_direita:Digito):
         self.__expressao_esquerda = expressao_esquerda
         self.__expressao_direita = expressao_direita
 
-    def interpretar(self) -> Numero:
+    def interpretar(self):
         return self.__expressao_esquerda.interpretar() - self.__expressao_direita.interpretar()
 
 class Multiplicar(AbsExpressao):
-    def __init__(self, expressao_esquerda: Numero, expressao_direita: Numero):
+    def __init__(self, expressao_esquerda:Digito, expressao_direita:Digito):
         self.__expressao_esquerda = expressao_esquerda
         self.__expressao_direita = expressao_direita
 
-    def interpretar(self) -> Numero:
+    def interpretar(self):
         return self.__expressao_esquerda.interpretar() * self.__expressao_direita.interpretar()
 
 
 if __name__ == '__main__':
 
     # ((5 + 5 - 3) * 10) + 10
-    #arvore ou pilha de expressoes
-    resultado = Soma(Multiplicar(Subtracao(Soma(Numero(5), Numero(5)), Numero(3)), Numero(10)), Numero(10))
+    # arvore de expressoes
+    resultado = Soma(Multiplicar(Subtracao(Soma(Digito(5), Digito(5)), Digito(3)), Digito(10)), Digito(10))
     print(resultado.interpretar())
 
-    resultado2 = Soma(resultado, Numero(80))
+    resultado2 = Soma(resultado, Digito(80))
     print(resultado2.interpretar())
-    
